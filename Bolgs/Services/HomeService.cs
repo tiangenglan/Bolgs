27dfe8b2ff6408f3795681ab804dd62db99abdb3
TianGenglan <zyy0306123@gmail.com>
2016-12-29 11:33:00 +0800

    Services/HomeService.cs

---
+++ Bolgs/Services/HomeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Gozyy.ToolClass.Models;
+using Gozyy.Repositories;
+namespace Bolgs.Services
+{
+    public class HomeService
+    {
+
+        public static List<Tbbolgs> List() 
+        {
+            var list = IRepository<Tbbolgs>.List("");
+            return list;
+        }
+    }
+}
